1617e4388f606e132786f41e78a56c50a76394d3
Roman <didukhroma@gmail.com>
2023-02-23 19:21:29 +0200

    test

--- src/js/kyu6/similarArrays.js
+++ src/js/kyu6/similarArrays.js
@@ -2,31 +2,40 @@ console.log('kyu-6-similarArrays');
 
 const arr1 = [1, 2, 2, 3, 4],
   arr2 = [2, 1, 2, 4, 3],
-  arr3 = [1, 2, 3, 4],
-  arr4 = [1, 2, 3, '4'];
+  arr3 = [1, 2, 3, 4];
+//   arr4 = [1, 2, 3, '4'];
 
 function arraysSimilar(arr1, arr2) {
   let result = false;
   let sortedArr1 = arr1.sort((a, b) => a - b);
   let sortedArr2 = arr2.sort((a, b) => a - b);
 
+  console.log(sortedArr1);
+  console.log(sortedArr2);
+
   if (sortedArr1.length !== sortedArr2.length) {
+    console.log(sortedArr1.length);
+    console.log(sortedArr2.length);
+    console.log('exit');
     return result;
   }
 
   for (let i = 0; i < sortedArr1.length; i++) {
-    if (sortedArr1[i] !== sortedArr2[i]) {
-      result = false;
-      break;
-    } else {
-      result = true;
-      continue;
-    }
+    console.log(sortedArr1[i] !== sortedArr2[i]);
+    // if (sortedArr1[i] !== sortedArr2[i]) {
+    //   result = false;
+    //   break;
+    // } else {
+    //   result = true;
+    // }
+    // continue;
+    // console.log(result);
   }
   // TODO: Implement your arraysSimilar
   return result;
 }
 
+console.log('----------------------');
 console.log(arr1);
 console.log(arraysSimilar(arr2, arr3));
-console.log(arraysSimilar(arr3, arr4));
+// console.log(arraysSimilar(arr3, arr4));
